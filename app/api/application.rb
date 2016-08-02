d72694fbf2b68f88dfbd63ab0df7f7eb2a26a631
iamsergey <noen@list.ru>
2016-08-02 20:33:03 +0300

    intend fixed

--- app/api/application.rb
+++ app/api/application.rb
@@ -11,7 +11,7 @@ module Api
     end
 
     resource :hotels do
-    	get :search do
+      get :search do
         query = "%#{params[:q].to_s.downcase}%"
 
         condition = <<~SQL
@@ -22,17 +22,17 @@ module Api
         hotels = Hotel.where(condition, query, '\\', query, '\\').all
 
         present hotels, with: Entities::Hotel
-    	end
+      end
 
-  		params do
-  			optional :limit, type: Integer
-  			optional :offset, type: Integer
-  		end
+      params do
+        optional :limit, type: Integer
+        optional :offset, type: Integer
+      end
 
-    	get do
-    		limit = params[:limit].to_i
-    		limit = 100 if limit == 0 || limit > 100
-      	hotels = Hotel.order(:id).reverse
+      get do
+        limit = params[:limit].to_i
+        limit = 100 if limit == 0 || limit > 100
+        hotels = Hotel.order(:id).reverse
                       .limit(limit)
                       .offset(params[:offset])
                       .all
@@ -61,7 +61,7 @@ module Api
         end
 
         put do
-        	hotel = Hotel[params[:id]] || not_found!
+          hotel = Hotel[params[:id]] || not_found!
           hotel.update name: params[:name],
                        address: params[:address],
                        accommodation_type: params[:accommodation_type]
@@ -70,7 +70,7 @@ module Api
         end
 
         delete do
-        	hotel = Hotel[params[:id]] || not_found!
+          hotel = Hotel[params[:id]] || not_found!
           hotel.destroy
           present hotel, with: Entities::Hotel
         end
